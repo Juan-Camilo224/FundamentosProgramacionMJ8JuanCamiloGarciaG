7148089c36ecc427274f4f2c2a67f20fbee1e641
Juan-Camilo224 <juan.garciaga@upb.edu.co>
2026-08-25 10:06:52 -0500

    7. Ciclo While

---
+++ 7.CicloWhile/7.CicloWhile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security;
+
+namespace _7.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    int contador = 1;
+            //    int acumulador = 1;
+            //    while (contador < 5)
+            //    {
+            //        contador++;
+            //        acumulador = acumulador + contador;
+            //    }
+            //    Console.WriteLine($"La suma de los primeros 5 numeros enteros es:"+ acumulador);
+
+            int acumulador = 1;
+            int contador = 1;
+            int numero = 1;
+            Console.WriteLine("Escriba un numero entero");
+            numero =int.Parse(Console.ReadLine());
+            while (contador > numero)
+            {
+                contador++;
+                acumulador = acumulador * contador;
+            }
+            Console.WriteLine(numero +" factorial es " + acumulador);
+        }
+    }
+}
