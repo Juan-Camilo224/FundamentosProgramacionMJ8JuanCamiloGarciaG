c9fe48e893736f7d4cd8a5bfa45264bfa7d7c477
Juan-Camilo224 <juan.garciaga@upb.edu.co>
2026-09-01 11:16:32 -0500

    11. CicloDowhile

---
+++ 11.CicloDoWhile/11.CicloDoWhile/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _11.CicloDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int contador = 1;
+            //int acumulador = 0;
+            //do
+            //{
+            //    acumulador += contador;
+            //    contador++;
+            //}while (contador <=5);
+            //Console.WriteLine("La suma de los cinco  primeros numeros enteros es " + acumulador);
+            
+            //int numero;
+            //int multiplicar;
+            //bool repetir;
+           
+            //do
+            //{
+            //    int contador = 1;
+            //    Console.WriteLine("Ingrese un numero para ver su tabla de multiplicar");
+            //    multiplicar = int.Parse(Console.ReadLine());
+            //    do
+            //    {
+
+            //        numero = contador * multiplicar;
+
+            //        Console.WriteLine(contador + " por " + multiplicar + " es igual a " + numero);
+            //        contador++;
+            //    } while (contador <= 10);
+            //    Console.WriteLine("Quieres ver otro? (true/false)");
+            //    repetir=bool.Parse(Console.ReadLine());
+
+            //} while (repetir=true);
+
+
+        }
+    }
+}
