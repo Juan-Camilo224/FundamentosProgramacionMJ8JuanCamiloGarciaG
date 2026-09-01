ccf9516a0b39ed61b7f4f5ba1e779f07ee0e51de
Juan-Camilo224 <juan.garciaga@upb.edu.co>
2026-09-01 11:15:42 -0500

    10.CiclosWhile3

---
+++ 10.CiclosWhile3/10.CiclosWhile3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _10.CiclosWhile3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Numero = new Random();
+            int Num= Numero.Next(1,101);
+            int NumH =-1;
+            Console.WriteLine("Adivina el numero que escogi");
+            while (Num != NumH) 
+            {
+                NumH=int.Parse(Console.ReadLine());
+                if (NumH > Num)
+                {
+                    Console.WriteLine("Demasiado alto");
+                }
+                if (NumH < Num)
+                {
+                    Console.WriteLine("Demasiado bajo");
+                }
+            }
+            Console.WriteLine("Acertaste");
+        }
+    }
+}
