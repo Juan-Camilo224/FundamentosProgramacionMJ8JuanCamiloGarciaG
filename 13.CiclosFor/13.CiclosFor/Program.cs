8f09ab3d95cae1877a8933e0e2afcfeb23e2a1fd
Juan-Camilo224 <juan.garciaga@upb.edu.co>
2026-09-03 09:28:53 -0500

    13.CiclosFor

---
+++ 13.CiclosFor/13.CiclosFor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _13.CiclosFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+            for (int contador = 1; contador <= 5; contador++)
+            {
+                acumulador += contador;
+            }
+            Console.WriteLine("La suma de los 5 primeros numeros es "+acumulador);
+        }
+    }
+}
