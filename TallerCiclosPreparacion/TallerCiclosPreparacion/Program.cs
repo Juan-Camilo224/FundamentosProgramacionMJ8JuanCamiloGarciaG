fc9cba030d70d084bd9fdf2d6ca66910a612acfd
Juan-Camilo224 <juan.garciaga@upb.edu.co>
2026-09-10 00:49:43 -0500

    TallerCiclosPreparacion

---
+++ TallerCiclosPreparacion/TallerCiclosPreparacion/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace TallerCiclosPreparacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Algoritmo que permita calcular el promedio de calificaciones, el algoritmo le permitirá al usuario, introducir
+            //tantas calificaciones como así desee, en el momento en que seleccione que no desea continuar capturando calificaciones,
+            //el algoritmo debe presentar el promedio de las calificaciones capturadas previamente.
+            //bool repetir = false;
+            //int NumNotas;
+            //int Nota;
+            //float Suma=0;
+            //float contador=0;
+            //    Console.WriteLine("Cuantas notas quiere evaluar?");
+            //    NumNotas = int.Parse(Console.ReadLine());
+            //    do
+            //    {
+            //        Console.WriteLine("Ingrese su nota");
+            //       Nota= int.Parse(Console.ReadLine());
+            //    Suma += Nota;
+            //        contador++;
+            //    Console.WriteLine("Quiere agregar otra nota? (true/false)");
+            //    repetir=bool.Parse(Console.ReadLine());
+            //    } while (repetir == true);
+            //Console.WriteLine("El promedio es " + Suma / contador);
+
+            //mostrar los divisores de un número ingresado.
+            //dos números enteros a y b, b es divisor de a si al efectuar una división entera a/b el residuo es 0, en C#
+            //utilizar el operador Mod para obtener el residuo de una división de dos números. Ejemplo: si se ingresa 6 por teclado, por pantalla
+            //se debe mostrar 6, 3, 2, 1 que son los divisores del número 6.
+
+            //float a = 1;
+            //float b = 1;
+            //float resultado = 1;
+            //Console.WriteLine("ingrese el dividendo");
+            //a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("ingrese el divisor");
+            //b = int.Parse(Console.ReadLine());
+            //for (int i = 0; i <b; i++)
+            //{
+
+            //    resultado = a / b;
+            //    if ( a % b == 0)
+            //    {
+            //        Console.WriteLine(resultado);
+            //    }
+            //}
+
+            //int potencia = 0;
+            //int basee = 0;
+            //bool Repetir = false;
+            //int multiplicacion=1;
+            //do
+            //{
+            //    Console.WriteLine("Ingrese su base");
+            //    basee = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("ingrese su exponete");
+            //    potencia = int.Parse(Console.ReadLine()); 
+            //    for (int contador = 0; contador < potencia; contador++)
+            //    {
+
+            //        multiplicacion *= basee;
+
+            //    }
+
+            //}while (Repetir == true );
+            //Console.WriteLine(multiplicacion);
+            //int dia;
+            //float suma = 0;
+            //bool ningunoMayorA20 = true;
+            //bool huboMenorA15 = false;
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("Ingrese el tiempo del día " + i);
+            //    dia = int.Parse(Console.ReadLine());
+
+            //    suma += dia;
+
+            //    if (dia > 20)
+            //    {
+            //        ningunoMayorA20 = false;
+            //    }
+            //    if (dia < 15)
+            //    {
+            //        huboMenorA15 = true;
+            //    }
+            //}
+
+            //float promedio = suma / 10;
+
+            //if (ningunoMayorA20 && huboMenorA15 && promedio <= 18)
+            //{
+            //    Console.WriteLine("Apto");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No apto");
+            //}
+            //int positivos = 0;
+            // int negativos = 0; 
+            //  int  neutros = 0;
+            //float sumaPositivos = 0, sumaNegativos = 0;
+            //int numero;
+
+            //for (int i = 1; i <= 40; i++)
+            //{
+            //    Console.WriteLine("Ingrese el número " + i);
+            //    numero = int.Parse(Console.ReadLine());
+
+            //    if (numero > 0)
+            //    {
+            //        positivos++;
+            //        sumaPositivos += numero;
+            //    }
+            //    else if (numero < 0)
+            //    {
+            //        negativos++;
+            //        sumaNegativos += numero;
+            //    }
+            //    else
+            //    {
+            //        neutros++;
+            //    }
+            //}
+
+            //Console.WriteLine("Positivos: " + positivos);
+            //Console.WriteLine("Negativos: " + negativos);
+            //Console.WriteLine("Neutros: " + neutros);
+            //Console.WriteLine("Suma de positivos: " + sumaPositivos);
+            //Console.WriteLine("Suma de negativos: " + sumaNegativos);
+
+        }
+    }
+}
