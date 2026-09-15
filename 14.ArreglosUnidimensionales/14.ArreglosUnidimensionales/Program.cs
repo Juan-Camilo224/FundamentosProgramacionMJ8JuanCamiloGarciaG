e89fd516abbd5c555fa043703dec1e85bf3ff0b2
Juan-Camilo224 <juan.garciaga@upb.edu.co>
2026-09-15 09:24:26 -0500

    14.ArreglosUnidimensionales

---
+++ 14.ArreglosUnidimensionales/14.ArreglosUnidimensionales/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _14.ArreglosUnidimensionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arreglos Unidimensionales o vectores
+            //int[] Numeros = new int[5];
+            //Numeros[0]=38;
+            //Numeros[1]=67;
+            //Numeros[2] = 23;
+            //Numeros[3] = 2;
+            //Numeros[4] = 21;
+            //Console.WriteLine("El dato almacenado en la posicion 4 es " + Numeros[3]);
+            //float [] notas = new float[3];
+            //notas[0] = 3.8f;
+            //notas[1] = 2.4f;
+            //notas[2] = 35.6f;
+            //char[] simbolos = new char[]{'w','r','g'};
+            //bool[] Verdades = new bool[] { true, false, true, false };
+            //string[] nombres = new string[7];
+            //for (int i = 0; i < 7; i++)
+            //{
+            //    Console.WriteLine($"ingrese el valor P{i + 1}: I{i}");
+            //    nombres[i]=Console.ReadLine();
+            //}
+            //for (int i = 0;i < nombres.Length;i++)
+            //{
+            //    Console.WriteLine($"{nombres[i]} |");
+            //}
+            int[] arreglo = new int[100];
+            for (int i = 0;i<100 ; i++)
+            {
+                Console.WriteLine(arreglo[i]=10);
+            }
+        }
+    }
+}
